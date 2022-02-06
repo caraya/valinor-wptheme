1db873a958bdbf95344c6bc3feb241bfa4a7da82
Carlos <carlos.araya@gmail.com>
2022-02-06 00:20:50 -0800

    added default font to front end working on other styles

--- functions.php
+++ functions.php
@@ -28,30 +28,71 @@ if ( ! function_exists( 'valinor_support' ) ) :
 endif;
 
 /**
+ * Enqueue font-related CSS
+ * 
+ * @since 1.0.0
+ */
+function valinor_enqueue_recursive_font() {
+  wp_enqueue_style(
+    'valinor-fonts',
+    get_template_directory_uri() . '/assets/css/recursive-styles.css',
+    false,
+    wp_get_theme()->get( 'Version' ),
+    'all'
+  );
+}
+
+/**
  * Enqueue scripts and styles.
  */
 function valinor_scripts() {
 
 	// Enqueue theme stylesheet.
-	wp_enqueue_style( 'valinor-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style(
+    'valinor-style',
+    get_template_directory_uri() . '/style.css',
+    array(),
+    wp_get_theme()->get( 'Version' )
+  );
 
-	// Enqueue alignments stylesheet.
-	wp_enqueue_style( 'valinor-alignments-style', get_template_directory_uri() . '/assets/css/alignments-front.css', array(), wp_get_theme()->get( 'Version' ) );
+  // Enqueue Editor styles
+  wp_enqueue_style(
+    'valinor-editor-style',
+    get_template_directory_uri() . '/assets/css/editor.css',
+    array(),
+    wp_get_theme()->get( 'Version' )
+  );
+	
+  // Enqueue alignments stylesheet.
+	wp_enqueue_style(
+    'valinor-alignments-style',
+    get_template_directory_uri() . '/assets/css/alignments-front.css',
+    array(),
+    wp_get_theme()->get( 'Version' )
+  );
 }
 add_action( 'wp_enqueue_scripts', 'valinor_scripts' );
 
 /**
  * Enqueue block editor script.
  *
- * @since 0.1
+ * @since 1.0.0
  *
  * @return void
  */
 function valinor_blocks_block_editor_script() {
 
-	wp_enqueue_script( 'valinor-blocks-unregister-block-style', get_theme_file_uri( '/assets/js/unregister-block-style.js' ), array( 'wp-blocks', 'wp-dom' ), wp_get_theme()->get( 'Version' ), true );
+	wp_enqueue_script(
+    'valinor-blocks-unregister-block-style', 
+    get_theme_file_uri( '/assets/js/unregister-block-style.js' ),
+    array( 'wp-blocks', 'wp-dom' ), 
+    wp_get_theme()->get( 'Version' ),
+    true );
 }
 add_action( 'enqueue_block_editor_assets', 'valinor_blocks_block_editor_script' );
+
+add_action( 'wp_enqueue_scripts', 'valinor_enqueue_recursive_font' );
+
 
 // Block Patterns.
 require get_template_directory() . '/inc/block-patterns.php';
