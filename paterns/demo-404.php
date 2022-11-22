5c364e63dcf20c506bbbe472d4c39713a7f40930
Carlos <carlos.araya@gmail.com>
2022-11-22 11:58:32 -0800

    added outline and element styles to theme.json

---
+++ paterns/demo-404.php
@@ -0,0 +1,11 @@
+<?php
+ /**
+  * Title: Hello
+  * Slug: my-theme/hello
+  * Categories: valinor patterns, text
+  */
+?>
+<main>
+  <p>Hello, world!</p>
+  <p>This is a demo from demo.404.php</p>
+</main>
